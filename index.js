3ea2aedc282c68430c8d8a884d8804d37a20b8ac
Jemima Siddiqui <jemimasiddiqui1@gmail.com>
2022-05-15 15:44:33 +1000

    added all the view options that can be selected by the user. The code is functional for all the view options

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const consoleTable = require("console.table");
 // Creating connection with database
 const connection = mysql.createConnection({
         host: "localhost",
-        //port: 3306,
+        port: 3306,
         user: "root",
-        password: "", // To enter your MySQL password here.
+        password: "Farkhanda7861!", // To enter your MySQL password here.
         database: "employees_db" // employee database 
     },
     console.log(`Connected to the courses_db database.`)
@@ -57,27 +57,31 @@ function runEmployeeDB() {
                 viewAllEmployees();
             break;
 
-            /*
+            
             // ** View all DEPARTMENTS **
             case "View All Departments":
-                viewAllDepts();
+                viewAllDepartments();
             break;
 
+            
              // ** View all ROLES **
             case "View All Roles":
                 viewAllRoles();
             break;
-                
+               
+            
             // ** View all EMPLOYEES BY DEPARTMENT **
             case "View All Employees by Department":
-                viewEmployeesByDept();
+                viewEmployeesByDepartment();
             break;
 
+            
             // ** View all EMPLOYEES BY ROLE **
             case "View All Employees by Role":
                 viewEmployeesByRole();
             break;
 
+            /*
             // ** Add a DEPARTMENT **
             case "Add Department":
                 addDept();
@@ -113,8 +117,7 @@ function runEmployeeDB() {
 
 // For when the user wants to view all employees 
 function viewAllEmployees() {
-    console.log("I am here"); 
-    /*connection.query("SELECT employees.firstName AS First_Name, employees.lastName AS Last_Name, role.title AS Title, role.salary AS Salary, department.name AS Department, CONCAT(e.firstName, ' ' ,e.lastName) AS Manager FROM employees INNER JOIN role on role.id = employees.roleID INNER JOIN department on department.id = role.departmentID LEFT JOIN employees e on employees.managerID = e.id;", 
+    connection.query("SELECT employees.firstName AS First_Name, employees.lastName AS Last_Name, role.title AS Title, role.salary AS Salary, department.name AS Department, CONCAT(e.firstName, ' ' ,e.lastName) AS Manager FROM employees INNER JOIN role on role.id = employees.roleID INNER JOIN department on department.id = role.departmentID LEFT JOIN employees e on employees.managerID = e.id;", 
     function(err, results) {
         if (err) {
             console.log(err);
@@ -122,12 +125,72 @@ function viewAllEmployees() {
         console.log(""); 
         console.log("** EMPLOYEES LIST **");
         console.log(""); 
-        //console.table(results)
+        console.table(results)
         runEmployeeDB()
-    })*/ 
-    connection.query("SELECT * FROM employees", function (err, results, fields) {
-        console.log(results);
-        console.log(fields);  
-    });
+    }) 
+    /*connection.query("SELECT * FROM employees", function (err, results, fields) {
+        console.table(results);
+        //process.exit(0); 
+        //console.log(fields);  
+    });*/ 
 }
 
+// For when the user wants to view all employee departments departments 
+function viewAllDepartments() {
+    connection.query("SELECT department.id AS ID, department.name AS Department FROM department", 
+    function(err, results) {
+        if (err) {
+            console.log(err);
+        }
+        console.log(""); 
+        console.log("** DEPARTMENT LIST **");
+        console.log(""); 
+        console.table(results)
+        runEmployeeDB()
+    }) 
+}
+
+// For when the user wants to view all the employee roles 
+function viewAllRoles() {
+    connection.query("SELECT role.id AS Deptartment_ID, role.title AS Title FROM role", 
+    function(err, results) {
+        if (err) {
+            console.log(err);
+        }
+        console.log(""); 
+        console.log("** ROLE LIST **");
+        console.log(""); 
+        console.table(results)
+        runEmployeeDB()
+    }) 
+}
+
+// For when the user wants to view employees by department 
+function viewEmployeesByDepartment() {
+    connection.query("SELECT employees.firstName AS First_Name, employees.lastName AS Last_Name, department.name AS Department FROM employees JOIN role ON employees.roleID = role.id JOIN department ON role.departmentID = department.id ORDER BY department.id;", 
+    function(err, results) {
+        if (err) {
+            console.log(err);
+        }
+        console.log(""); 
+        console.log("** EMPLOYEES BY DEPARTMENT LIST **");
+        console.log(""); 
+        console.table(results)
+        runEmployeeDB()
+    }) 
+}
+
+// For when the user wants to view employees by role 
+function viewEmployeesByRole() {
+    connection.query("SELECT employees.firstName AS First_Name, employees.lastName AS Last_Name, role.title AS Title FROM employees JOIN role ON employees.roleID = role.id ORDER BY role.id;", 
+    function(err, results) {
+        if (err) {
+            console.log(err);
+        }
+        console.log(""); 
+        console.log("** EMPLOYEES BY DEPARTMENT LIST **");
+        console.log(""); 
+        console.table(results)
+        runEmployeeDB()
+    }) 
+}
