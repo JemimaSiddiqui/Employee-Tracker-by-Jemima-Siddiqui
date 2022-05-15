5733ba907e5f898f4b3223232c150176758e113e
Jemima Siddiqui <jemimasiddiqui1@gmail.com>
2022-05-15 13:06:43 +1000

    created employee database with employee, department and role tables

---
+++ schema.sql
@@ -0,0 +1,27 @@
+-- Creating employee database 
+DROP DATABASE IF EXISTS employees.db;
+CREATE DATABASE employees_db; 
+USE employees_db; 
+
+-- Creating EMPLOYEE table 
+CREATE TABLE employees (
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    firstName VARCHAR(30), 
+    lastName VARCHAR(30), 
+    roleID INT, 
+    managerID INT 
+); 
+
+-- Creating DEPARTMENT table 
+CREATE TABLE department (
+    id INT(11) PRIMARY KEY, 
+    name VARCHAR(30)
+); 
+
+-- Creating ROLE table 
+CREATE TABLE role (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY, 
+    title VARCHAR(30), 
+    salary DECIMAL(9,2), 
+    departmentID INT 
+); 
